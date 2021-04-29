9468004aaf3de9a113dfff1482278f9109c2fc4e
Jorge <jorgediazok@gmail.com>
2021-04-29 02:20:42 -0300

    progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Progress from './Progress';
 
-const Header = () => {
-  return <h1>Multi Step Registration</h1>;
-};
+const Header = () => (
+  <div>
+    <h1>Multi Step Registration</h1>
+    <Progress />
+  </div>
+);
 
 export default Header;
